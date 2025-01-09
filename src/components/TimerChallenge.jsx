c0829c7adfc4150212024c504d96340dab7e7ee0
Aaradhya Poudyal <aaradhyapoudyal@Aaradhyas-MacBook-Air.local>
2025-01-09 13:58:19 -0800

    added functionality to start timer and display appropriate UI changes

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    //setTimeout is a default jS feature
+    //setTimeout works in milliseconds but we are dealing with seconds, that's why we multiply with 1000
+
+    //if the above function runs, time is expired hence the player lost
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
 
       <p>
-        <button>Start Challenge Stop Challenge</button>
+        <button onClick={handleStart}>
+          {" "}
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is running OR Timer Inactive</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? 'Time is running' : 'Timer Stopped'}
+      </p>
     </section>
   );
 }
