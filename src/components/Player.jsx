94907e182600789a1ec208444d6baa7d04ed5c07
Aaradhya Poudyal <aaradhyapoudyal@Aaradhyas-MacBook-Air.local>
2025-01-09 12:58:42 -0800

    Deleted submitted useState. Added first Ref

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,24 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+//useRef is a hook as well 
 export default function Player() {
+  const playerName = useRef();
+  //playerName will always be a jS object 
+  //it will always have a current and only a current property 
+  
   const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-  function handleChange(event) {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
 
   function handleClick() {
-    setSubmitted(true);
+    //inside the current property, the actual ref value will be stored
+    setEnteredPlayerName(playerName.current.value) ;
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName + ' :]' : 'unknown entity'}</h2>
+      <h2>
+        Welcome {enteredPlayerName ?? "unknown entity"}
+      </h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <input ref={playerName}type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
